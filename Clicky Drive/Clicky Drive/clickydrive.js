ffa7b8494920924557601d0cdf841c6ffb355014
qwerty4967 <jmcraft126@gmail.com>
2020-12-05 22:51:07 -0500

    v0.1.1.128

--- Clicky Drive/Clicky Drive/clickydrive.js
+++ Clicky Drive/Clicky Drive/clickydrive.js
@@ -2,67 +2,80 @@ var ClickyDrive =
 {
 	
 	game:undefined,
-        versionString:"Clicky Drive v0.1.0.121 ",
+        versionString:"Clicky Drive v0.1.1.128 ",
 	versionAppend:"",
 	versionWatermark:undefined,
-	vWatermarkX:0, // nope,  not anymore.
+	vWatermarkX:0, // in 'pixels'
 	vWatermarkY:0,
 	vWatermarkStyle:{ fontFamily: '"Arial"', fontSize:'12pt', color:'white', strokeThickness:0 },
-     	aspectRatio:16/9,
+    aspectRatio:16/9,
+	
 	background:undefined,
 	ui:undefined,
 
 	preload: function()
 	{ 
-		 console.log(ClickyDrive.versionString);
-		 this.load.image('bg',ClickyDrive.background ); 
-		 this.load.html('ui', ClickyDrive.ui);
+		console.log(ClickyDrive.versionString);
+		
+		if(ClickyDrive.background!=undefined)
+		{
+			this.load.image('bg',ClickyDrive.background ); 
+		}
+		
+		if(ClickyDrive.ui!=undefined)
+		{
+			this.load.html('ui', ClickyDrive.ui);
+		}
 	},
 		
 
 	create:function()
 	{
 		
-		ClickyDrive.background = this.add.image(800,450, 'bg'); // must be in 16:9 aspect ratio.
-		
-		if (ClickyDrive.aspectRatio != ClickyDrive.background.width/ClickyDrive.background.height)
+		//background
+		if(ClickyDrive.background!=undefined)
 		{
-		   throw "Background Aspect Ratio MUST be " + ClickyDive.aspectRatio +" width/height!"
+			ClickyDrive.background = this.add.image(800,450, 'bg'); // must be in 16:9 aspect ratio.
+			
+			if (ClickyDrive.aspectRatio != ClickyDrive.background.width/ClickyDrive.background.height)
+			{
+			   throw "Background Aspect Ratio MUST be " + ClickyDive.aspectRatio +" width/height!"
+			}
+			// resize the background.
+			ClickyDrive.background.setScale(config.scale.height/ClickyDrive.background.height);
 		}
-		// resize the background.
-                ClickyDrive.background.setScale(config.scale.height/ClickyDrive.background.height);
-
-		// Watermark things properly.
-		ClickyDrive.versionWatermark = this.add.text(ClickyDrive.vWatermarkX, ClickyDrive.vWatermarkY, ClickyDrive.versionString+ClickyDrive.versionAppend, ClickyDrive.vWatermarkStyle);
-    		ClickyDrive.versionWatermark.originX=-.5; // make it easier to deal with
-		ClickyDrive.versionWatermark.originY=-.5;
+		
 		
 		
 		// add in a dom
-		//this.add.dom(0, 0, 'div', 'background-color: lime; width: 220px; height: 100px; font: 48px Arial', 'Phaser');
-		this.add.dom(0,0).createFromHTML(ClickyDrive.ui);
-
+		if(ClickyDrive.ui!=undefined)
+		{
+			this.add.dom(0,0).createFromHTML(this.cache.html.get('ui'));
+		}
+		
+		// Watermark things properly.
+		ClickyDrive.versionWatermark = this.add.text(ClickyDrive.vWatermarkX, ClickyDrive.vWatermarkY, ClickyDrive.versionString+ClickyDrive.versionAppend, ClickyDrive.vWatermarkStyle);
+    	ClickyDrive.versionWatermark.originX=-.5; // make it easier to deal with
+		ClickyDrive.versionWatermark.originY=-.5;
 
 	},
 	
 	update:function()
 	{
-		// let's try to only resize after resizing is done, to prevent lag. Don't do it every frame during user resizing.
-	
+
+		//sweet nothing...
 	
 	}
 
 	
 }
 
+// config is last because the functions are defined in an object
 config =
 {
    type: Phaser.AUTO,
    autoCenter: true,
-   dom:
-   {
-     createContainer: true
-   },
+   dom:{createContainer: true},
    parent: 'phaser-parent',
    scale: 
    {
@@ -85,6 +98,3 @@ config =
 	
 	
 }
-
-
-
