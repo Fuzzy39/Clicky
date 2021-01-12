e90fbf61897318f7dfa946d99c6993f319759cb4
You are using git for this? huh. What happens If I delete this file? does it do nothing? probably. Maybe it makes it slightly inconventient for you next year. probably not though <2023hillms@sa.hccsc.us>
2021-01-11 20:00:55 -0500

    gui start

--- Clicky 3.1/game.js
+++ Clicky 3.1/game.js
@@ -1,8 +1,7 @@
 "use strict";
-// Style the version Watermark
-let version = "134";
-console.log("test #"+version);
-ClickyDrive.versionAppend="Running Test #"+version;
+
+
+ClickyDrive.versionAppend="| Clicky 3.1";
 
 // Select Background image
 ClickyDrive.background='Assets/Background.png';
@@ -18,6 +17,7 @@ let gold = new ClickyDrive.resource("gold", 30, true);
 
 // define a node to go along with it.
 let goldNode =new ClickyDrive.node("gold",800,450, ["Assets/gold0.png","Assets/gold1.png","Assets/gold2.png","Assets/gold3.png"]);
+goldNode.size=320;
 
 // set up fragments.
 goldNode.fragment="Assets/goldFrag.png";
@@ -29,16 +29,16 @@ goldNode.glow.texture="Assets/Glow.png";
 goldNode.glow.scale=3;
 
 let prospect = new ClickyDrive.item('prospect', {'gold':5}, 1, {"gold":0});
-prospect.onPurchase= function() { gold.add(1000); }
+//prospect.onPurchase= function() { gold.add(1000); }
 
 let miner = new ClickyDrive.item('miner', {'gold':10}, 1.1, {"gold":10});
 
-ClickyDrive.hookins.update = function()
+/*ClickyDrive.hookins.update = function()
 {
 	ClickyDrive.ui.getChildByID("gold").innerHTML="Gold: "+prettyPrint(gold.amount)+" | GPS: "+ gold.perSecond;
 	ClickyDrive.ui.getChildByID("miner").innerHTML="Miner ("+miner.costs.gold+"g)";
 	ClickyDrive.ui.getChildByID("amount").innerHTML="Owned: "+miner.amount;
-}
+}*/
 
 // start the game	
 ClickyDrive.game = new Phaser.Game(config);
