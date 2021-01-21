688327f221dfd5ce0c12afeb42b07ca9b1e7f3b4
You are using git for this? huh. What happens If I delete this file? does it do nothing? probably. Maybe it makes it slightly inconventient for you next year. probably not though <2023hillms@sa.hccsc.us>
2021-01-21 17:46:35 -0500

    Unlock System

--- Clicky 4/game.js
+++ Clicky 4/game.js
@@ -27,8 +27,87 @@ goldNode.glow.texture="Assets/Glow.png";
 goldNode.glow.scale=3;
 
 
-//let prospect = new ClickyDrive.item('prospect', {'gold':5}, 1, {"gold":0});
-//prospect.onPurchase= function() { gold.add(1000); }
+// Set enums for all of the buttons
+// ""Enums""
+// Whatever.
+let unlockIDs =
+{ 
+    	// So this only counts for the buttons that need unlocked, 
+	//and has only the button IDs, not the children, there will be an update one, okay?
+	PickUp:0,
+	MinerUp:1,
+	DrillUp:2,
+	LazerUp:3,
+	Prospect:4,
+	Prospector:5,
+	Miner:6,
+	Drill:7,
+	Lazer:8
+}
+
+
+
+function updateUnlocked()
+{
+	for( let i in unlockIDs )
+	{
+		if(!isUnlocked(unlockIDs[i]))
+		{
+			
+			switch(unlockIDs[i])
+			{
+				
+				case unlockIDs.PickUp:
+					if(gold.amount>=/*pickup.cost*/10){unlock(unlockIDs[i]);}
+				break;
+					
+			}
+			
+			
+				
+		}
+
+	}
+}
+
+// returns boolean.
+function isUnlocked( unlockID )
+{
+	for( let i in unlockIDs )
+	{
+		if(unlockID==i)
+		{	
+			// nonsense.
+			if(document.getElementById(Object.keys(unlockIDs)[i])==null)
+			{
+				// seems bizare, but whatever
+				return true;
+			}
+
+			if(!document.getElementById(Object.keys(unlockIDs)[i]).classList.contains('hidden'))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+				
+		}
+	}
+	return false;
+}
+
+function unlock(unlockID)
+{
+	let element = document.getElementById(Object.keys(unlockIDs)[unlockID]);
+	element.classList.remove('hidden');
+}
+
+
+//let pickUp = new ClickyDrive.item('prospect', {'gold':10}, 3, {"gold":0});
+//pickUp.onPurchase= function() { gold.perClick*=2;}
+
 
 //let miner = new ClickyDrive.item('miner', {'gold':10}, 1.1, {"gold":10});
 
@@ -37,8 +116,9 @@ ClickyDrive.hookins.update = function()
 	ClickyDrive.ui.getChildByID("gold").innerHTML="Gold: "+prettyPrint(gold.amount)+inlineIcon;
 	ClickyDrive.ui.getChildByID("gps").innerHTML=gold.perSecond+" GPS";
 	
-	//ClickyDrive.ui.getChildByID("miner").innerHTML="Miner ("+miner.costs.gold+"g)";
-	//ClickyDrive.ui.getChildByID("amount").innerHTML="Owned: "+miner.amount;
+	 updateUnlocked();
+	
+
 }
 
 // start the game	
